5bdd5838f3e2f65530c5e88381e342fd486c0873
Andy O'Neill <aoneill01@gmail.com>
2021-01-18 20:42:18 -0500

    Make lines thicker

--- static/scripts/constants.js
+++ static/scripts/constants.js
@@ -3,9 +3,9 @@ export const endFadeSeconds = 18;
 
 export const lineOpacity = 204; // out of 255
 export const lineWidth = {
-  small: 5,
-  medium: 10,
-  large: 20,
+  small: 10,
+  medium: 20,
+  large: 40,
 };
 
 export const minDistance = 5; // How far apart line points must be
